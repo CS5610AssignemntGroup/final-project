76a46c623f33788f1fcdf6998a346f1f2b13cbde
crispgit <zhoupj1215@hotmail.com>
2020-12-07 14:08:20 -0800

    update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Foot, NavBar } from "./components";
+import { NavBar } from "./components";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import {
   HomePage,
