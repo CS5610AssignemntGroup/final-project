0a5e7bc55f89b68b029a109f7f500f989e8222be
M Z <man@ms-macbook-pro.lan>
2020-12-09 13:14:50 -0800

    fix format

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,14 @@ const bodyParser = require('body-parser');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const Product = require('./models/product')
+const Product = require('./models/product');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use(
-    '/graphql', 
+    '/graphql',
     graphqlHTTP({
         schema: buildSchema(`
             type Product {
@@ -48,55 +48,63 @@ app.use(
                 query: RootQuery
                 mutation: RootMutation
             }
-        `),  
+        `),
         rootValue: {
             getAllProducts: () => {
                 return Product.find()
                     .then(products => {
-                        return products.map(product => { 
-                            return {...product._doc, _id: product.id }; 
-                        })
-                    }).catch(err => {
+                        return products.map(product => {
+                            return { ...product._doc, _id: product.id };
+                        });
+                    })
+                    .catch(err => {
                         throw err;
                     });
-            }, 
+            },
             addProduct: args => {
                 const product = new Product({
                     // user: args.productInput.user,
-                    image: args.productInput.image, 
+                    image: args.productInput.image,
                     brand: args.productInput.brand,
-                    category: args.productInput.category, 
-                    description: args.productInput.description, 
-                    rating: args.productInput.rating, 
-                    reviewCount: args.productInput.reviewCount, 
+                    category: args.productInput.category,
+                    description: args.productInput.description,
+                    rating: args.productInput.rating,
+                    reviewCount: args.productInput.reviewCount,
                     price: +args.productInput.price,
-                    stockCount: args.productInput.stockCount, 
+                    stockCount: args.productInput.stockCount,
                     // reviews: args.productInput.reviews
                 });
 
-                return product.save().then(result => {
-                    console.log(result);
+                return product
+                    .save()
+                    .then(result => {
+                        console.log(result);
 
-                    // return result;
-                    return {...result._doc, _id: product.id};
-                }).catch(err => {
-                    console.log(err);
-                    throw err;
-                });
-            }
-        }, 
+                        // return result;
+                        return { ...result._doc, _id: product.id };
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        throw err;
+                    });
+            },
+        },
         graphiql: true,
     })
 );
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.0md4f.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.0md4f.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+    )
     .then(() => {
-        app.listen(process.env.PORT || 4000, () => console.log(`server start at port: ${process.env.PORT}`))
+        app.listen(process.env.PORT || 4000, () =>
+            console.log(`server start at port: ${process.env.PORT}`)
+        );
     })
     .catch(err => {
         console.log(err);
     });
-
 
 /**
  * 
