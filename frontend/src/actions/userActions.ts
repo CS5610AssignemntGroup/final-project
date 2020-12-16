f0de55b9900ce8815cbcc4a49aad2b72287b8258
crispgit <zhoupj1215@hotmail.com>
2020-12-16 13:10:00 -0800

    add user login and register actions and reducers

---
+++ frontend/src/actions/userActions.ts
@@ -0,0 +1,130 @@
+import axios from 'axios';
+
+export const login = (email: string, password: string) => async (
+    dispatch: (arg0: { type: any; payload?: any }) => void
+) => {
+    try {
+        dispatch({
+            type: 'USER_LOGIN_REQUEST',
+        });
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        const { data } = await axios.post(
+            '/api/users/login',
+            { email, password },
+            config
+        );
+
+        dispatch({
+            type: 'USER_LOGIN_SUCCESS',
+            payload: data,
+        });
+
+        localStorage.setItem('userInfo', JSON.stringify(data));
+    } catch (error) {
+        dispatch({
+            type: 'USER_LOGIN_FAIL',
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+export const logout = () => (dispatch: (arg0: { type: string }) => void) => {
+    localStorage.removeItem('userInfo');
+    dispatch({ type: 'USER_LOGOUT' });
+    dispatch({ type: 'USER_DETAILS_RESET' });
+    document.location.href = '/login';
+};
+
+export const register = (
+    name: string,
+    email: string,
+    password: string
+) => async (dispatch: (arg0: { type: string; payload?: any }) => void) => {
+    try {
+        dispatch({
+            type: 'USER_REGISTER_REQUEST',
+        });
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        const { data } = await axios.post(
+            '/api/users',
+            { name, email, password },
+            config
+        );
+
+        dispatch({
+            type: 'USER_REGISTER_SUCCESS',
+            payload: data,
+        });
+
+        dispatch({
+            type: 'USER_LOGIN_SUCCESS',
+            payload: data,
+        });
+
+        localStorage.setItem('userInfo', JSON.stringify(data));
+    } catch (error) {
+        dispatch({
+            type: 'USER_REGISTER_FAIL',
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+export const getUserDetails = (id: string) => async (
+    dispatch: (arg0: { type?: string; payload?: any }) => void,
+    getState: () => { userLogin: { userInfo: any } }
+) => {
+    try {
+        dispatch({
+            type: 'USER_DETAILS_REQUEST',
+        });
+
+        const {
+            userLogin: { userInfo },
+        } = getState();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        };
+
+        const { data } = await axios.get(`/api/users/${id}`, config);
+
+        dispatch({
+            type: 'USER_DETAILS_SUCCESS',
+            payload: data,
+        });
+    } catch (error) {
+        const message =
+            error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message;
+        if (message === 'Not authorized, token failed') {
+            // @ts-ignore
+            dispatch(logout());
+        }
+        dispatch({
+            type: 'USER_DETAILS_FAIL',
+            payload: message,
+        });
+    }
+};
---
+++ frontend/src/reducers/userReducers.ts
@@ -0,0 +1,56 @@
+// @ts-ignore
+export const userLoginReducer = (
+    state = {},
+    action: { type: any; payload: any }
+) => {
+    switch (action.type) {
+        case 'USER_LOGIN_REQUEST':
+            return { loading: true };
+        case 'USER_LOGIN_SUCCESS':
+            return { loading: false, userInfo: action.payload };
+        case 'USER_LOGIN_FAIL':
+            return { loading: false, error: action.payload };
+        case 'USER_LOGOUT':
+            return {};
+        default:
+            return state;
+    }
+};
+
+// @ts-ignore
+export const userRegisterReducer = (
+    state = {},
+    action: { type: any; payload: any }
+) => {
+    switch (action.type) {
+        case 'USER_REGISTER_REQUEST':
+            return { loading: true };
+        case 'USER_REGISTER_SUCCESS':
+            return { loading: false, userInfo: action.payload };
+        case 'USER_REGISTER_FAIL':
+            return { loading: false, error: action.payload };
+        case 'USER_LOGOUT':
+            return {};
+        default:
+            return state;
+    }
+};
+
+// @ts-ignore
+export const userDetailsReducer = (
+    state = { user: {} },
+    action: { type: any; payload: any }
+) => {
+    switch (action.type) {
+        case 'USER_DETAILS_REQUEST':
+            return { ...state, loading: true };
+        case 'USER_DETAILS_SUCCESS':
+            return { loading: false, user: action.payload };
+        case 'USER_DETAILS_FAIL':
+            return { loading: false, error: action.payload };
+        case 'USER_DETAILS_RESET':
+            return { user: {} };
+        default:
+            return state;
+    }
+};
