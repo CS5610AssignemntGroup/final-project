79b2eae146f014df01db77187fa7014b02f45678
crispgit <zhoupj1215@hotmail.com>
2020-12-16 21:08:19 -0800

    add search page

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { logout } from '../../actions/userActions';
 import { RootState } from '../../types';
+import { SearchBox } from '../../components';
 
 interface OwnProps {}
 
@@ -27,35 +28,51 @@ const Header: FunctionComponent<Props> = props => {
                     <LinkContainer to="/">
                         <Navbar.Brand href="/">AceBook</Navbar.Brand>
                     </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Route
+                            render={({ history }) => (
+                                <SearchBox history={history} />
+                            )}
+                        />
 
-                    <Nav className="ml-auto">
-                        {userInfo ? (
-                            <NavDropdown title={userInfo.name} id="username">
-                                <LinkContainer to="/profile">
-                                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                        <Nav className="ml-auto">
+                            {userInfo ? (
+                                <NavDropdown
+                                    title={userInfo.name}
+                                    id="username">
+                                    <LinkContainer to="/profile">
+                                        <NavDropdown.Item>
+                                            Profile
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>
+                                        Logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to="/login">
+                                    <Nav.Link>
+                                        <i className="fas fa-user"></i> Sign In
+                                    </Nav.Link>
                                 </LinkContainer>
-                                <NavDropdown.Item onClick={logoutHandler}>
-                                    Logout
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        ) : (
-                            <LinkContainer to="/login">
-                                <Nav.Link>
-                                    <i className="fas fa-user"></i> Sign In
-                                </Nav.Link>
-                            </LinkContainer>
-                        )}
-                        {userInfo && userInfo.isAdmin && (
-                            <NavDropdown title="Admin" id="adminmenu">
-                                <LinkContainer to="/admin/userlist">
-                                    <NavDropdown.Item>Users</NavDropdown.Item>
-                                </LinkContainer>
-                                <LinkContainer to="/admin/booklist">
-                                    <NavDropdown.Item>Books</NavDropdown.Item>
-                                </LinkContainer>
-                            </NavDropdown>
-                        )}
-                    </Nav>
+                            )}
+                            {userInfo && userInfo.isAdmin && (
+                                <NavDropdown title="Admin" id="adminmenu">
+                                    <LinkContainer to="/admin/userlist">
+                                        <NavDropdown.Item>
+                                            Users
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to="/admin/booklist">
+                                        <NavDropdown.Item>
+                                            Books
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                </NavDropdown>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </header>
--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -2,3 +2,4 @@ export { BookCard } from './BookCard/BookCard';
 export { Header } from './Header/Header';
 export { Footer } from './Footer/Footer';
 export { Rating } from './Rating/Rating';
+export { SearchBox } from './SearchBox/SearchBox';
